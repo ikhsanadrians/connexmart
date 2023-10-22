599addbecb6b3da95c7cf92dd787e7798a98d7c9
ikhsanadrians <aikhsan032@gmail.com>
2023-10-22 18:23:35 +0700

    chore: add search input components

--- resources/views/admin/adduser.blade.php
+++ resources/views/admin/adduser.blade.php
@@ -82,8 +82,6 @@
         </div>
     </div>
 </div>
-
-
     <div class="headers flex justify-between">
         <h1 class="text-2xl font-bold">Add User</h1>
         <button id="openaddmodal" class="add-user bg-[#003034] text-white px-3 py-2 rounded-lg flex items-center gap-2">
@@ -98,6 +96,19 @@
                     d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
             </svg>
         </button>
+    </div>
+    <div class="searchandfilter flex items-center gap-3">
+        <div class="search mt-2 relative flex items-center">
+            <svg class="absolute left-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            <input type="text" placeholder="Search User" class="pl-8 pr-4 py-2 rounded-md focus:outline-none">
+        </div>
+        <div class="filter bg-[#003034] text-white mt-2 p-[11px] rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16">
+                <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"/>
+              </svg>
+        </div>
     </div>
     <div class="user-list w-full mt-8 mb-8">
         <table class="w-full">
