316712598ab6d830a4c6af3a4b011d625cf4b4c6
ikhsanadrians <aikhsan032@gmail.com>
2023-10-26 23:22:29 +0700

    format: mart controller format prettier

--- app/Http/Controllers/MartController.php
+++ app/Http/Controllers/MartController.php
@@ -10,101 +10,111 @@ use Illuminate\Support\Facades\Auth;
 
 class MartController extends Controller
 {
-   public function index(){
-     $products = Product::all();
-     return view('mart.index', compact('products'));
-   }
+  public function index()
+  {
+    $products = Product::all();
+    return view('mart.index', compact('products'));
+  }
 
-   public function goodsindex(){
-     $products = Product::all();
-     $productcategories = Category::all();
-     return view('mart.goods',compact('products','productcategories'));
-   }
+  public function goodsindex()
+  {
+    $products = Product::all();
+    $productcategories = Category::all();
+    return view('mart.goods', compact('products', 'productcategories'));
+  }
 
-   public function goodpost(Request $request){
-     
-       $goods = Product::create([
-         'name' => $request->name,
-         'price' => $request->price,
-         'stock' => $request->stock,
-         'photo' => 'photo',
-         'desc' => $request->description,
-         'category_id' => $request->category_id,
-         'stand' => 2,
-       ]);
+  public function goodpost(Request $request)
+  {
 
-       alert()->success('Success','Success Add New Product!');
+    $goods = Product::create([
+      'name' => $request->name,
+      'price' => $request->price,
+      'stock' => $request->stock,
+      'photo' => 'photo',
+      'desc' => $request->description,
+      'category_id' => $request->category_id,
+      'stand' => 2,
+    ]);
 
-       return redirect()->route('mart.goods');
+    alert()->success('Success', 'Success Add New Product!');
 
-   }
+    return redirect()->route('mart.goods');
 
-   public function goodsupdate(Request $request){
-        if($request->ajax()){
-          $goodsToUpdate = Product::find($request->product_id);
-          
-          $goodsToUpdate->update([
-            "name" => $request->product_name,
-            "price" => $request->product_price,
-            "stock" => $request->product_stock,
-            "photo" => "photo",
-            "desc" => $request->product_description,
-            "category_id" => $request->product_categoryid,
-            "stand" => 2
-          ]);
+  }
 
-          alert()->success("Success","Success Update Product");
-          
-          return response()->json([
-             "message" => "Success Update Data!",
-             "data" => $goodsToUpdate
-          ]);
+  public function goodsupdate(Request $request)
+  {
+    if ($request->ajax()) {
+      $goodsToUpdate = Product::find($request->product_id);
 
-        }
-   }
+      $goodsToUpdate->update([
+        "name" => $request->product_name,
+        "price" => $request->product_price,
+        "stock" => $request->product_stock,
+        "photo" => "photo",
+        "desc" => $request->product_description,
+        "category_id" => $request->product_categoryid,
+        "stand" => 2
+      ]);
 
-   public function goodsdelete(Request $request){
-if($request->ajax()) {
-  $deletedProduct = Product::find($request->id_to_delete);
- 
-  alert()->success("Success","Success Delete Product");
-          
-  return response()->json([
-    "message" => "Success Delete Data!",
-    "data" => $deletedProduct
-  ]);
+      alert()->success("Success", "Success Update Product");
 
-      }
-   }
+      return response()->json([
+        "message" => "Success Update Data!",
+        "data" => $goodsToUpdate
+      ]);
 
-   public function addcategory(){
-     return view('mart.category');
-   }
+    }
+  }
+
+  public function goodsdelete(Request $request)
+  {
+    if ($request->ajax()) {
+      $deletedProduct = Product::find($request->id_to_delete);
+
+      alert()->success("Success", "Success Delete Product");
+
+      return response()->json([
+        "message" => "Success Delete Data!",
+        "data" => $deletedProduct
+      ]);
+
+    }
+  }
+
+  public function addcategory()
+  {
+    return view('mart.category');
+  }
 
 
-   public function auth(){
+  public function auth()
+  {
     return view("mart.login");
-}
+  }
 
-public function auth_proceed(Request $request){
+  public function auth_proceed(Request $request)
+  {
     $credentials = [
-        "name" => $request->username,
-        "password" => $request->password
+      "name" => $request->username,
+      "password" => $request->password
     ];
 
-    $checkRoles =  User::where('name',$credentials['name'])->first();
+    $checkRoles = User::where('name', $credentials['name'])->first();
 
-    if(Auth::attempt($credentials)) return redirect()->route('mart.index');
+    if (Auth::attempt($credentials))
+      return redirect()->route('mart.index');
 
     return redirect()->back();
-}
+  }
 
-public function martlogout(){
+  public function martlogout()
+  {
     Auth::logout();
 
     request()->session()->invalidate();
 
     return redirect()->route('mart.auth');
-}
+  }
 
 }
